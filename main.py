503c8e28c55af3d77129a93a89de7db686c22340
alihaghour <alihaghour15@gmail.com>
2021-11-14 00:18:04 -0500

    Updated main and organized code

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 import pygame
 from spritesheet import Spritesheet
 
-################################################################################################################################
 
 #CONSTANTS
 WIDTH, HEIGHT = 720, 364
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 60        
+FPS = 60
+
+#WINDOW INIT
 pygame.display.set_caption("First Game!")
-spritesheet = Spritesheet('./Assets/fullss.png')
 
-ss1 = spritesheet.get_sprite(0, 32, 32, 3, (0,0,0))
-
-
-
-#BACKGROUND
-def draw_background():
-
-    pygame.display.update()
-
-################################################################################################################################
+#VARIABLES
+clock = pygame.time.Clock()
+run = True
 
 #Main Function with event handler
 def main():
-    clock = pygame.time.Clock()
-    run = True
 
-    #Loop to handle events (LOCKED TO UPDATE AT 60FPS)
+    #Event Handler
     while run:
-        WIN.fill((50,50,50))
-        WIN.blit(ss1, (0,0))
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
--- player.py
+++ player.py
@@ -1,9 +1,12 @@
 import pygame
 from spritesheet import Spritesheet
 
+#SPRITESHEET IMPORT
+spritesheet = Spritesheet('./Assets/fullss.png')
+ss1 = spritesheet.get_sprite(0, 32, 32, 3, (0,0,0))
 
-#Player Object
-
+#Player Class
 class Player:
-    def __init__(self):
-        
+    def __init__(self, pos_x, pos_y):
+        self.sprite = []
+        self.sprite.append()
